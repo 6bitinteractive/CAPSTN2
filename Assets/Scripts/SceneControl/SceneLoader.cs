7c07983e1b4e72f9d89f6bf7610bca3c1d7290c0
Lim <michelle.lim@benilde.edu.ph>
2019-05-26 15:21:57 +0800

    Add scene loader;
    Add application quit

---
+++ Assets/Scripts/SceneControl/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private SceneData sceneDataToLoad;
+
+    private static SceneController sceneController;
+    private static SaveData playerSaveData;
+
+    private void Start()
+    {
+        if (sceneController == null)
+            sceneController = SingletonManager.GetInstance<SceneController>();
+
+        if (playerSaveData == null)
+            playerSaveData = sceneController.PlayerSaveData;
+    }
+
+    public void LoadScene()
+    {
+        sceneController.FadeAndLoadScene(sceneDataToLoad.SceneName);
+    }
+}
