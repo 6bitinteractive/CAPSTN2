3cc1f242383cda737a65907ce5d087b905324480
Azunyan Taichou <meitanteinep@gmail.com>
2019-05-26 19:31:11 +0800

    Add timer related scripts

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public float TimerValue = 10;
+    public UnityEvent OnCountdownEnd;
+
+    private float currentTimerValue;
+
+    public float CurrentTimerValue
+    {
+        get
+        {
+            return currentTimerValue;
+        }
+
+        set
+        {
+            currentTimerValue = value;
+        }
+    }
+
+    void Start()
+    {
+        if (OnCountdownEnd == null) OnCountdownEnd = new UnityEvent();
+
+        CurrentTimerValue = TimerValue;
+        StartCoroutine(StartCountdown());
+    }
+
+    void Update()
+    {
+        // If countdown ends call CountdownEnd function
+        if (CurrentTimerValue <= 0)
+        {
+            CurrentTimerValue = 0f;
+            CountdownEnd();
+        }
+    }
+
+    public IEnumerator StartCountdown()
+    {
+        while (CurrentTimerValue > 0)
+        {
+            yield return new WaitForSeconds(Time.deltaTime); // Scale timer
+            CurrentTimerValue -= Time.deltaTime; // Reduce timer
+        }
+    }
+
+    private void CountdownEnd()
+    {
+        if (OnCountdownEnd != null) OnCountdownEnd.Invoke(); // Broadcast to listeners that countdown has ended
+    }
+
+    public void ResetTimer()
+    {
+        CurrentTimerValue = TimerValue;
+        StartCoroutine(StartCountdown());
+    }
+}
---
+++ Assets/Scripts/TimerUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+
+    private Timer timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Timer>();
+
+        timerText.text = timer.CurrentTimerValue.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer.TimerValue >= 0f)
+        {
+            timerText.text = timer.CurrentTimerValue.ToString("F2");
+        }
+    }
+}
