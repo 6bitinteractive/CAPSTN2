64c158f305a55e24a97d478983991b35935f7368
Azunyan Taichou <meitanteinep@gmail.com>
2019-05-29 14:35:23 +0800

    Add customer script

---
+++ Assets/Scripts/Customer/Customer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    [SerializeField] float PatienceTimer;
+    private Timer timer;
+
+    public enum FSMState
+    {
+        Idle,
+        Queuing,
+        Ordering,
+        Waiting,
+        Eating,
+        Leaving
+    }
+
+    public FSMState curState; // current state
+
+    private void Awake()
+    {
+        timer = GetComponent<Timer>();
+        timer.TimerValue = PatienceTimer; // Set the timer
+
+        curState = FSMState.Queuing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(curState)
+        {
+            case FSMState.Idle: IdleState(); break;
+            case FSMState.Queuing: QueuingState(); break;
+            case FSMState.Ordering: OrderingState(); break;
+            case FSMState.Waiting: WaitingState(); break;
+            case FSMState.Eating: EatingState(); break;
+            case FSMState.Leaving: LeavingState(); break;
+        }
+    }
+
+    private void IdleState()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void QueuingState()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OrderingState()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void WaitingState()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EatingState()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void LeavingState()
+    {
+        throw new NotImplementedException();
+    }
+}
