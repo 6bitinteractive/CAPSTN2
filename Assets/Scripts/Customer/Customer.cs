7faa451df4916b0ba672b127a7e774ac6bc7c915
Azunyan Taichou <meitanteinep@gmail.com>
2019-06-02 11:32:45 +0800

    Add notes to customer's fsm script

--- Assets/Scripts/Customer/Customer.cs
+++ Assets/Scripts/Customer/Customer.cs
@@ -4,15 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(Timer))]
+
 public class Customer : MonoBehaviour
 {
     [SerializeField] float PatienceTimer;
     [SerializeField] float Speed = 0.02f;
 
     public int myQueue;
-    public Vector3 destination;
+    public Vector3 Destination;
 
+    private Chair MyChair;
     private Timer timer;
+    private DroppableToChair droppableToChair;
     private Vector3 startingPosition;
     private bool isInQueue = false;
 
@@ -30,15 +36,11 @@ public class Customer : MonoBehaviour
 
     public FSMState curState; // current state
 
+    public Chair MyChair1 { get => MyChair; set => MyChair = value; }
+
     private void Awake()
     {
-        timer = GetComponent<Timer>();
-        timer.TimerValue = PatienceTimer; // Set the timer
-        timer.enabled = false; // Disable timer
-
-        startingPosition = transform.position;
-
-        curState = FSMState.Queuing;
+        Init();
     }
 
     // Update is called once per frame
@@ -57,7 +59,8 @@ public class Customer : MonoBehaviour
 
     private void IdleState()
     {
-    //    throw new NotImplementedException();
+        // Play an idling animation
+        // throw new NotImplementedException();
     }
 
     private void QueuingState()
@@ -67,21 +70,34 @@ public class Customer : MonoBehaviour
 
     private void OrderingState()
     {
-        throw new NotImplementedException();
+        timer.ResetTimer();
+        Destroy(gameObject.GetComponent<DroppableToChair>(),1);
+        // Order for a set duration
+        // Select an order
+        // Wait for player click to advance to waiting state
+        //Debug.Log("Im ordering");
     }
 
     private void WaitingState()
     {
+        // Reset Patience Timer
+        // Able to accept order
+        // Upon accepting order go to eating state
         throw new NotImplementedException();
     }
 
     private void EatingState()
     {
+        // Eat for a set duration
+        // Upon finished eating move to leaving state
         throw new NotImplementedException();
     }
 
     private void LeavingState()
     {
+        // Earn Score
+        // Set the isOccupied for MyChair to false
+        // Customer dissappears through an effect or walks out?
         throw new NotImplementedException();
     }
 
@@ -89,18 +105,40 @@ public class Customer : MonoBehaviour
     {
         while (!isInQueue)
         {
-            transform.position = Vector2.MoveTowards(transform.position, destination, Speed * Time.deltaTime);
-            transform.right = destination - transform.position; // Rotate Towards destination
+            MoveUpInQueue();
 
-            if (transform.position.x >= destination.x)
+            if (transform.position.x >= Destination.x)
             {
                 isInQueue = true;
                 timer.enabled = true;
+                gameObject.AddComponent<DroppableToChair>(); // Hack for some reason even when the script is disabled player can still drag the object
                 OnEnterQueue.Invoke();
                 curState = FSMState.Idle;
                 yield break;
             }
             yield return 0;
         }
+    }
+
+    public void ResetObject()
+    {
+        MyChair1 = null;
+    }
+
+    public void MoveUpInQueue()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Destination, Speed * Time.deltaTime);
+        transform.right = Destination - transform.position; // Rotate Towards destination
+    }
+
+    private void Init()
+    {
+        timer = GetComponent<Timer>();
+        timer.TimerValue = PatienceTimer; // Set the timer
+        timer.enabled = false; // Disable timer
+
+        startingPosition = transform.position;
+
+        curState = FSMState.Queuing;
     }
 }
