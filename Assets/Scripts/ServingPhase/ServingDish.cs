b60c6b4eaf7e5f9a5ce0768978841820c211308b
Azunyan Taichou <meitanteinep@gmail.com>
2019-06-10 08:32:46 +0800

    Fix a sprite rendering bug that occurs only in build

--- Assets/Scripts/ServingPhase/ServingDish.cs
+++ Assets/Scripts/ServingPhase/ServingDish.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class ServingDish : MonoBehaviour
 {
    public Order OrderType;
+
+   void Awake()
+   {
+      gameObject.GetComponent<SpriteRenderer>().sprite = OrderType.Sprite;
+    }
 }
