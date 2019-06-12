1e9dd93eb848554d23866014ec6c6676592c4663
Lim <michelle.lim@benilde.edu.ph>
2019-06-12 22:01:10 +0800

    Fix DragToPot prompt moving its position when meat is dragged;
    Add empty script for more complex tap/swipe gesture-based steps

---
+++ Assets/Scripts/CookingPhase/Steps/Actions/FancySliceAndDiceAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FancySliceAndDiceAction : Action
+{
+    public override void Begin()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ResetAction()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool SuccessConditionMet()
+    {
+        throw new System.NotImplementedException();
+    }
+}
