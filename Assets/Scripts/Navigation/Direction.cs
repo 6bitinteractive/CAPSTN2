aafd2de06f506c28014abb8c54ced586681425df
Azunyan Taichou <meitanteinep@gmail.com>
2019-06-24 21:57:02 +0800

    Add direction script

---
+++ Assets/Scripts/Navigation/Direction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Direction : MonoBehaviour
+{
+    public bool FacingRight = true;
+    public Vector3 scale;
+
+    void Start()
+    {
+        scale = transform.localScale;
+    }
+
+    public void CheckDirection(float direction)
+    {
+        // Checks direction and determines if the sprite should flip or not
+        if (direction > 0 && !FacingRight)
+        {
+            scale.x = -scale.x;
+            FacingRight = true;
+        }
+        else if (direction < 0 && FacingRight)
+        {
+            scale.x = -scale.x;
+            FacingRight = false;
+        }
+        transform.localScale = scale;
+    }
+}
