20263926706c6fec06b6a129f822effdba4aee8d
Lim <michelle.lim@benilde.edu.ph>
2019-06-29 22:07:20 +0800

    Add StoveController, FlameAnimationController

---
+++ Assets/Scripts/CookingPhase/StoveArea/Stove/FlameAnimationController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Animator))]
+
+public class FlameAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        animator = GetComponent<Animator>();
+
+        // Turn off flame
+        //image.enabled = false; // NOTE: Current art implementation has no "off" setting
+    }
+
+    public void UpdateFlame(HeatSetting heatSetting)
+    {
+        image.enabled = true;
+
+        switch (heatSetting)
+        {
+            case HeatSetting.Low:
+                animator.SetInteger("State", 1);
+                break;
+            case HeatSetting.Medium:
+                animator.SetInteger("State", 2);
+                break;
+            case HeatSetting.High:
+                animator.SetInteger("State", 3);
+                break;
+            default:
+                animator.SetInteger("State", 0);
+                //image.enabled = false;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/CookingPhase/StoveArea/Stove/StoveController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+
+[System.Serializable]
+public class OnStoveSettingChanged : UnityEvent<HeatSetting> { }
+
+public class StoveController : MonoBehaviour
+{
+    [Range(0, 1)]
+    [SerializeField] private float maxLow = 0.3f, maxMedium = 0.7f;
+
+    public HeatSetting CurrentHeatSetting { get; private set; }
+    public OnStoveSettingChanged OnStoveSettingChanged = new OnStoveSettingChanged();
+
+    public void UpdateStoveSetting(float sliderValue)
+    {
+        if (sliderValue == 0f)
+            //CurrentHeatSetting = HeatSetting.Off;
+            CurrentHeatSetting = HeatSetting.Low; // NOTE: Current art implementation has no "off" setting
+        else if (sliderValue > 0 && sliderValue <= maxLow)
+            CurrentHeatSetting = HeatSetting.Low;
+        else if (sliderValue <= maxMedium && sliderValue > maxLow)
+            CurrentHeatSetting = HeatSetting.Medium;
+        else
+            CurrentHeatSetting = HeatSetting.High;
+
+        OnStoveSettingChanged.Invoke(CurrentHeatSetting);
+    }
+}
+
+public enum HeatSetting
+{
+    Off, Low, Medium, High
+}
