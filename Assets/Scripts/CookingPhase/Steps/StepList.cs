52db10ed891ba97e1a4544ca57130dfad25501b1
Lim <michelle.lim@benilde.edu.ph>
2019-06-30 18:54:23 +0800

    Add (temporary) panel for list of steps

---
+++ Assets/Scripts/CookingPhase/Steps/StepList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StepList : MonoBehaviour
+{
+    private List<Button> stepButtons;
+    private Animator animator;
+
+    private void Awake()
+    {
+        stepButtons = new List<Button>();
+        stepButtons.AddRange(GetComponentsInChildren<Button>());
+
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        foreach (var stepButton in stepButtons)
+            stepButton.onClick.AddListener(FadeOutPanel);
+    }
+
+    private void OnDisable()
+    {
+        foreach (var stepButton in stepButtons)
+            stepButton.onClick.RemoveListener(FadeOutPanel);
+    }
+
+    public void FadeOutPanel()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void FadeInPanel()
+    {
+        animator.SetTrigger("FadeIn");
+    }
+}
---
+++ Assets/Scripts/Utilities/AnimatorUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorUtils : MonoBehaviour
+{
+    public static bool IsDonePlaying(Animator animator, string state, int layer = 0, float normalizedTime = 1f)
+    {
+        return animator.GetCurrentAnimatorStateInfo(layer).normalizedTime >= normalizedTime;
+    }
+
+    private static bool IsInState(Animator animator, string state, int layer = 0)
+    {
+        return animator.GetCurrentAnimatorStateInfo(layer).IsName(state);
+    }
+}
