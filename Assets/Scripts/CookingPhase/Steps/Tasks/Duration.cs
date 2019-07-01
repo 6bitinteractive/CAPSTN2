5b4da44aca71857b60f0db88e807e8144d6c061d
Lim <michelle.lim@benilde.edu.ph>
2019-07-01 17:18:08 +0800

    Add TaskUI (timer, animation)

---
+++ Assets/Scripts/CookingPhase/Steps/Tasks/Duration.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Duration : MonoBehaviour
+{
+    public float CurrentTime;
+    public float MaxDuration = 10f;
+    public bool Countdown = true;
+    public bool End { get; private set; }
+
+    public UnityEvent OnTimerStart = new UnityEvent();
+    public UnityEvent OnTimerEnd = new UnityEvent();
+    public UnityEvent OnTimerUpdate = new UnityEvent();
+
+    private void Start()
+    {
+        if (Countdown)
+            CurrentTime = MaxDuration;
+    }
+
+    public IEnumerator StartTimer()
+    {
+        Debug.Log("Timer started.");
+        OnTimerStart.Invoke();
+
+        while (!End)
+        {
+            if (Countdown)
+            {
+                CurrentTime -= Time.deltaTime;
+
+                if (CurrentTime <= 0f)
+                    EndTimer();
+            }
+            else
+            {
+                CurrentTime += Time.deltaTime;
+
+                if (CurrentTime >= MaxDuration)
+                    EndTimer();
+            }
+
+            OnTimerUpdate.Invoke();
+
+            //Debug.Log("Current time: " + CurrentTime);
+            yield return null;
+        }
+    }
+
+    private void EndTimer()
+    {
+        End = true;
+        OnTimerEnd.Invoke();
+    }
+}
---
+++ Assets/Scripts/CookingPhase/Steps/Tasks/TaskUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Duration))]
+
+public class TaskUI : MonoBehaviour
+{
+    [SerializeField] private GameObject taskImageHidden;
+    [SerializeField] private GameObject taskImageRevealed;
+    [SerializeField] private Image timerBg;
+
+    private Duration duration;
+
+    private void Awake()
+    {
+        duration = GetComponent<Duration>();
+    }
+
+    private void OnEnable()
+    {
+        duration.OnTimerUpdate.AddListener(UpdateTimerBg);
+    }
+
+    private void OnDisable()
+    {
+        duration.OnTimerUpdate.RemoveListener(UpdateTimerBg);
+    }
+
+    private void UpdateTimerBg()
+    {
+        if (duration.MaxDuration > 0)
+            timerBg.fillAmount = Mathf.Clamp01(duration.CurrentTime / duration.MaxDuration);
+    }
+}
