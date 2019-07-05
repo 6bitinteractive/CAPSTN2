390ae6620603a2a452a0b8cb3f4a8c4f4c4a2013
Lim <michelle.lim@benilde.edu.ph>
2019-07-05 10:08:48 +0800

    Add check to make sure SuccessConditionMet() returns the correct output

--- Assets/Scripts/CookingPhase/Steps/Tasks/TaskVariants/AddIngredientDragTask.cs
+++ Assets/Scripts/CookingPhase/Steps/Tasks/TaskVariants/AddIngredientDragTask.cs
@@ -6,6 +6,12 @@ public class AddIngredientDragTask : Task
 {
     [SerializeField] private List<GameObject> ingredientsRequiredInCookware;
 
+    protected override void Setup()
+    {
+        if (ingredientsRequiredInCookware.Count == 0)
+            Debug.LogError(gameObject.name + ": No ingredient(s) in required list.");
+    }
+
     protected override bool SuccessConditionMet()
     {
         // If one ingredient hasn't been enabled (ie. not in the cookware), the action is considered a failure
