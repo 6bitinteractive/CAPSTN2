2b4d2a362b47b62f1ce308c89459362976fb8af6
Azunyan Taichou <meitanteinep@gmail.com>
2019-07-06 15:53:03 +0800

    Add InteractiveObject script

---
+++ Assets/Scripts/Adventure/InteractiveObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class InteractiveObject : MonoBehaviour
+{
+    [SerializeField] UnityEvent OnInteract = new UnityEvent();
+    [SerializeField] UnityEvent OnNPCCollision = new UnityEvent();
+    [SerializeField] UnityEvent OnNPCExit = new UnityEvent();
+    [SerializeField] Image InteractSymbol;
+    [SerializeField] Canvas canvas;
+
+    private void Awake()
+    {
+        canvas.enabled = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If player
+        if (collision.gameObject.layer == 8)
+        {
+            canvas.enabled = true;
+            OnNPCCollision.Invoke();
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        // If player
+        if (collision.gameObject.layer == 8)
+        {
+            canvas.enabled = false;
+        }
+    }
+
+    public void Talk()
+    {
+        OnInteract.Invoke();
+    }
+}
