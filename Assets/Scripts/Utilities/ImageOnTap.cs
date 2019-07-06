ab225b1cbf5f921e1aa50fb9d940bf23c41a2e82
Lim <michelle.lim@benilde.edu.ph>
2019-07-06 22:20:33 +0800

    Add ImageOnTap script

---
+++ Assets/Scripts/Utilities/ImageOnTap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+
+public class ImageOnTap : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Sprite defaultSprite;
+    [SerializeField] private Sprite onTapSprite;
+
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        ShowDefault();
+    }
+
+    private void OnMouseDown()
+    {
+        ShowOnTap();
+    }
+
+    private void OnMouseUp()
+    {
+        ShowDefault();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ShowOnTap();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        ShowDefault();
+    }
+
+    private void ShowOnTap()
+    {
+        image.sprite = onTapSprite;
+    }
+
+    private void ShowDefault()
+    {
+        image.sprite = defaultSprite;
+    }
+}
