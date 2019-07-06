619ac24909bc4fe5a16cd3b448c445b4dc7e9e05
Lim <michelle.lim@benilde.edu.ph>
2019-07-06 11:03:01 +0800

    Add debug panel to show rating/comment

--- Assets/Scripts/CookingPhase/Steps/StepRating.cs
+++ Assets/Scripts/CookingPhase/Steps/StepRating.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class StepRating : MonoBehaviour
 {
+    [Header("Debug")]
+    [SerializeField] private TextMeshProUGUI ratingText;
+    [SerializeField] private TextMeshProUGUI commentText;
+
+    [Space]
+
     [SerializeField] private TaskManager taskManager;
     [HideInInspector] public int Rating = RecipeRating.MAX_RATING;
     [HideInInspector] public List<string> Comments = new List<string>();
@@ -17,12 +24,16 @@ public class StepRating : MonoBehaviour
 
     private void OnEnable()
     {
+        taskManager.OnAllTasksDone.AddListener(DisplayEvaluation);
+
         foreach (var task in taskManager.Tasks)
             task.OnEnd.AddListener(Evaluate);
     }
 
     private void OnDisable()
     {
+        taskManager.OnAllTasksDone.RemoveListener(DisplayEvaluation);
+
         foreach (var task in taskManager.Tasks)
             task.OnEnd.RemoveListener(Evaluate);
     }
@@ -30,9 +41,15 @@ public class StepRating : MonoBehaviour
     private void Evaluate(Task task)
     {
         Rating += task.Successful ? 0 : -1; // If the task failed, rating decrease by 1
-        Rating = Mathf.Clamp(Rating, 0, RecipeRating.MAX_RATING); // In case tasks have more than five steps and player fails more than five times
+        Rating = Mathf.Clamp(Rating, 0, RecipeRating.MAX_RATING); // In case tasks have more than the number of maxRating and player fails more than maxRating
         Comments.Add(task.Successful ? task.RatingComment.SuccessText : task.RatingComment.FailText);
 
         Debug.Log(task.gameObject.name + "Added Comment: " + (task.Successful ? task.RatingComment.SuccessText : task.RatingComment.FailText));
+    }
+
+    private void DisplayEvaluation()
+    {
+        ratingText.text = string.Format("RATING: {0} / {1}", Rating.ToString(), RecipeRating.MAX_RATING);
+        commentText.text = string.Format("Comments: {0}", string.Join(" ", Comments));
     }
 }
