65d9b62a453a8244691a178bd08d51c2224469eb
Lim <michelle.lim@benilde.edu.ph>
2019-07-11 17:31:19 +0800

    Add CookingOverview scene

---
+++ Assets/Scripts/CookingPhase/CookingOverview/Step.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Step : MonoBehaviour
+{
+    public string DisplayName;
+    public bool Locked = true;
+    public int Rating;
+}
---
+++ Assets/Scripts/CookingPhase/CookingOverview/StepUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Step))]
+
+public class StepUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI stepNameText;
+    [SerializeField] private GameObject[] starRatingImages;
+
+    private Step step;
+
+    private void Start() // Do this at start to make sure the step object has loaded its data (eg. rating)
+    {
+        step = GetComponent<Step>();
+
+        // [Debug] Display the name of the step
+        stepNameText.text = step.DisplayName;
+
+        // If there's a rating for the step, show same number of stars
+        if (step.Rating > 0)
+        {
+            for (int i = 0; i < step.Rating; i++)
+                starRatingImages[i].SetActive(true);
+        }
+    }
+}
