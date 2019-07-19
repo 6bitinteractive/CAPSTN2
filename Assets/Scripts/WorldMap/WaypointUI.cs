c10a04a5875b355573b9a06a82aa5afe7a671527
Azunyan Taichou <meitanteinep@gmail.com>
2019-07-19 18:50:16 +0800

    Add WaypointUI script

---
+++ Assets/Scripts/WorldMap/WaypointUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System;
+
+[RequireComponent(typeof(Waypoint))]
+
+[System.Serializable] public class OnWayPointArrival : UnityEvent<SceneData> { }
+
+public class WaypointUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI waypointNameText;
+
+    [HideInInspector] public OnWayPointArrival OnWaypointArrival = new OnWayPointArrival();
+
+    private Waypoint waypoint;
+
+    private void Start()
+    {
+        waypoint = GetComponent<Waypoint>();
+
+        if (waypoint.Locked)
+            gameObject.SetActive(false);
+
+            waypointNameText.text = waypoint.DisplayName;
+    }
+
+    public void InvokeOnWaypointArrival()
+    {
+        OnWaypointArrival.Invoke(waypoint.StageScene);
+    }
+}
