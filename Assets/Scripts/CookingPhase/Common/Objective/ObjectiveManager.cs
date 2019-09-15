ecca639290a1ced754c0130d216960a69261ae83
Lim <michelle.lim@benilde.edu.ph>
2019-09-15 12:25:53 +0800

    Add event to broadcast when all objectives are done

--- Assets/Scripts/CookingPhase/Common/Objective/ObjectiveManager.cs
+++ Assets/Scripts/CookingPhase/Common/Objective/ObjectiveManager.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ObjectiveManager : MonoBehaviour
 {
     public List<Objective> Objectives { get; private set; }
     private int currentObjective;
+
+    public UnityEvent OnAllObjectivesDone = new UnityEvent();
 
     private void Awake()
     {
@@ -26,7 +29,14 @@ public class ObjectiveManager : MonoBehaviour
     {
         Objectives[currentObjective].End();
         currentObjective++;
-        if (currentObjective >= Objectives.Count) { return; }
-        else Objectives[currentObjective].gameObject.SetActive(true);
+
+        if (currentObjective >= Objectives.Count)
+        {
+            Debug.Log("All objectives done.");
+            OnAllObjectivesDone.Invoke();
+            return;
+        }
+
+        Objectives[currentObjective].gameObject.SetActive(true);
     }
 }
