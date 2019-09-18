ec578fcb7faa71d612d491d164c0317eed1cf7b8
Lim <michelle.lim@benilde.edu.ph>
2019-09-18 08:46:03 +0800

    Revert "Add detecting start/end of swipe when touch moves/stationary"
    This reverts commit 97c4c4d7bb8947d8058133856e39841d49926cc6.

--- Assets/Scripts/Input/SwipeDetector.cs
+++ Assets/Scripts/Input/SwipeDetector.cs
@@ -46,11 +46,9 @@ public class SwipeDetector : MonoBehaviour
             switch (touch.phase)
             {
                 case TouchPhase.Began:
-                case TouchPhase.Moved:
                     SetStartPosition(touch.position);
                     break;
 
-                case TouchPhase.Stationary:
                 case TouchPhase.Ended:
                 case TouchPhase.Canceled:
                     SetEndPosition(touch.position);
