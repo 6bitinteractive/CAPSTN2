9d5c40960be21fb02b2f26855393988a1ef0ba98
Azunyan Taichou <meitanteinep@gmail.com>
2019-09-28 23:41:04 +0800

    Add swipeable script

---
+++ Assets/Scripts/Utilities/Swipeable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Swipeable : MonoBehaviour
+{
+    public SwipeDirection SwipeDirection;
+    public UnityEvent OnCorrectSwipe = new UnityEvent();
+    public SwipeEvent OnCorrectSwipeDirection = new SwipeEvent();
+
+    private SwipeDirectionListener swipeDirectionListener;
+    private bool inputInCorrectArea;
+    private RectTransform rectTransform;
+
+    private void OnEnable()
+    {
+        swipeDirectionListener = SingletonManager.GetInstance<SwipeDirectionListener>();
+        SwipeDetector.OnSwipe += VerifySwipe;
+        OnCorrectSwipeDirection.AddListener(swipeDirectionListener.InvokeOnCorrectSwipe);
+    }
+
+    private void VerifySwipe(SwipeData swipeData)
+    {
+        Debug.Log("Nep");
+        if (swipeData.Direction == SwipeDirection.None)
+            return;
+
+        if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, swipeData.StartPosition))
+        {
+            inputInCorrectArea = true;
+            Debug.Log("a");
+        }
+
+        if (swipeData.Direction == SwipeDirection)
+        {
+            OnCorrectSwipeDirection.Invoke(swipeData);
+            OnCorrectSwipe.Invoke();
+            Debug.Log("b");
+        }
+    }
+}
