9fbe05a6a4befb1d203c68441b37e0eb0255b280
Lim <michelle.lim@benilde.edu.ph>
2019-10-05 00:23:25 +0800

    Add PreheatCookwareAddIngredientObjective

---
+++ Assets/Scripts/CookingPhase/Common/Objective/ObjectiveTypes/PreheatCookwareAddIngredientObjective.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreheatCookwareAddIngredientObjective : Objective
+{
+    [SerializeField] private List<Cookable> ingredientsToBeAdded;
+    [SerializeField] private GameObject steam;
+    [SerializeField] private float steamWaitTime = 5f;
+
+    private bool preheated, addedBeforePreheated;
+    private int currentIngredient;
+
+    [SerializeField] private ObjectiveState perfectState = new ObjectiveState(ObjectiveState.Status.Perfect);
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // Setup objectives
+        // Add to list
+        ObjectiveStates.Add(perfectState);
+
+        // Define condition
+        perfectState.HasBeenReached = () => preheated;
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        // Listen to event if previous ingredient has been dropped
+        foreach (var ingredient in ingredientsToBeAdded)
+        {
+            ingredient.OnIngredientDroppedToCookware.AddListener(AddNextIngredient);
+        }
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+
+        foreach (var ingredient in ingredientsToBeAdded)
+        {
+            ingredient.OnIngredientDroppedToCookware.RemoveAllListeners();
+        }
+    }
+
+    protected override void InitializeObjective()
+    {
+        if (ingredientsToBeAdded.Count == 0)
+            Debug.LogError("Specify which ingredients are to be added.");
+
+        // Enable first ingredient
+        ingredientsToBeAdded[currentIngredient].gameObject.SetActive(true);
+
+        Invoke("LetOutSteam", steamWaitTime);
+    }
+
+    protected override void FinalizeObjective()
+    {
+        base.FinalizeObjective();
+
+        foreach (var ingredient in ingredientsToBeAdded)
+        {
+            ingredient.gameObject.SetActive(false);
+        }
+    }
+
+    protected override bool SuccessConditionMet()
+    {
+        bool success = true; // We start with true in case there's only one item
+
+        foreach (var ingredient in ingredientsToBeAdded)
+        {
+            // Check each ingredient if it's in the cookware; it can only be successful if *all* ingredients are in the cookware
+            success = success && kitchen.Cookware.CookableIngredients.Exists(x => x.IngredientInCookware == ingredient.IngredientInCookware);
+        }
+
+        return success && !addedBeforePreheated;
+    }
+
+    private void AddNextIngredient()
+    {
+        currentIngredient++;
+
+        // Check if player added an ingredient before steam came out
+        if (!preheated)
+            addedBeforePreheated = true;
+
+
+        if (currentIngredient >= ingredientsToBeAdded.Count)
+        {
+            GoToNextObjective(true);
+            return;
+        }
+
+        ingredientsToBeAdded[currentIngredient].gameObject.SetActive(true);
+    }
+
+    private void LetOutSteam()
+    {
+        steam.SetActive(true);
+        preheated = true;
+    }
+}
