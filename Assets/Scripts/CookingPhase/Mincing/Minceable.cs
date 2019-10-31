5a37ff638d251a3b859c8e49e16066b1d798a5ab
Azunyan Taichou <meitanteinep@gmail.com>
2019-10-31 15:09:10 +0800

    Add "Minceable" script

---
+++ Assets/Scripts/CookingPhase/Mincing/Minceable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minceable : MonoBehaviour
+{
+    [SerializeField] private RuntimeAnimatorController[] animatorControllers;
+
+    private Animator animator;
+    private int currentAnimatorController;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+   public void UpdateCurrentAnimatorController()
+    {
+        animator.runtimeAnimatorController = animatorControllers[currentAnimatorController++];
+    }
+}
