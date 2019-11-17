6ae21a436c1d786d0f54c515bda009fe87a27da1
Lim <michelle.lim@benilde.edu.ph>
2019-11-17 17:34:41 +0800

    Add CapizWindow Title for Pamapanga exploration scene

---
+++ Assets/Scripts/CookingPhase/Capiz/CapizWindowTitle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapizWindow))]
+
+public class CapizWindowTitle : MonoBehaviour
+{
+    [SerializeField] private float delayOpen = 2f;
+    [SerializeField] private Animator act;
+    [SerializeField] private Animator title;
+
+    private CanvasGroup canvasGroup;
+    private CapizWindow capizWindow;
+
+    private void Awake()
+    {
+        capizWindow = GetComponent<CapizWindow>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(OpenScene());
+    }
+
+    private IEnumerator OpenScene()
+    {
+        yield return new WaitForSeconds(delayOpen);
+        capizWindow.Display(false);
+        act.SetTrigger("FadeOut");
+        title.SetTrigger("FadeOut");
+    }
+}
