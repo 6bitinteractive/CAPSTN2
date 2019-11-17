e984cdcdca5a798364beea9f4190d3cc316fd7b6
Lim <michelle.lim@benilde.edu.ph>
2019-11-17 11:18:34 +0800

    Add anahaw animation; fix timing

---
+++ Assets/Scripts/CookingPhase/Capiz/CapizWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CapizWindow : MonoBehaviour
+{
+    [SerializeField] private Animator leftWindow;
+    [SerializeField] private Animator rightWindow;
+    [SerializeField] private Animator anahaw;
+
+    public void Display(bool display = true)
+    {
+        leftWindow.SetTrigger(display ? "SlideIn" : "SlideOut");
+        rightWindow.SetTrigger(display ? "SlideIn" : "SlideOut");
+        anahaw.SetTrigger(display ? "Enter" : "Exit");
+    }
+}
